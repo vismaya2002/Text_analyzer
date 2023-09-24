8d740dce37c89016f438ee25cf439487432b09b3
vismaya2002 <vismay2002@gmail.com>
2023-09-24 23:39:15 +0530

    created word count

--- text/views.py
+++ text/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-
+import shlex
 
 # Create your views here.
 
 def text_analyzer(request):
+    context = {
+        'status': False
+    }
     if request.method == 'POST':
-        pass
-        
-    return render(request,'index.html')
+        text = request.POST.get('text_main')
+        word_count = len(shlex.split(text))
+        print(word_count)
+        context = {
+            'status' : True,
+            'word_count' : word_count,
+            'text' : text
+        }
+    return render(request,'index.html',context)
 
